e8c29cb67ea9be52760b07f9e6aa67e763548b95
Samson Ajayi <Samson.Ajayi@sterling.ng>
2022-08-04 09:19:18 +0100

    Add project files.

---
+++ EcommerceBlazor/Server/Data/DataContext.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace EcommerceBlazor.Server.Data
+{
+    public class DataContext:DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options):base(options)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Product>().HasData(
+          new Product
+            {
+                Id = 1,
+                Title = "The Hitchhiker's Guide to the Galaxy",
+                Description = "The Hitchhiker's Guide to the Galaxy [Note 1] (sometimes you have to travel the stars)",
+                ImageUrl = "https://upload.wikimedia.org/wikipedia/en/b/bd/H2G2_UK_front_cover.jpg",
+                Price = 9.99m
+            },
+         new Product
+         {
+             Id = 2,
+             Title = "Ready Player One",
+             Description = "Ready Player One is a 2011 science fiction novel, and the debut novel for the best player in America",
+             ImageUrl = "https://upload.wikimedia.org/wikipedia/en/a/a4/Ready_Player_One_cover.jpg",
+             Price = 7.99m
+         },
+         new Product
+         {
+             Id = 3,
+             Title = "Ninteen Eighty-Four",
+             Description = "Ninteen Eighty-Four (also stylised as 1984) is a native store of USA background",
+             ImageUrl = "https://upload.wikimedia.org/wikipedia/commons/c/c3/1984first.jpg",
+             Price = 9.99m
+         }
+         );
+        }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
---
+++ EcommerceBlazor/Server/Migrations/20220731120022_ProductSeeding.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EcommerceBlazor.Server.Migrations
+{
+    public partial class ProductSeeding : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<decimal>(
+                name: "Price",
+                table: "Products",
+                type: "decimal(18,2)",
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(18,2");
+
+            migrationBuilder.InsertData(
+                table: "Products",
+                columns: new[] { "Id", "Description", "ImageUrl", "Price", "Title" },
+                values: new object[] { 1, "The Hitchhiker's Guide to the Galaxy [Note 1] (sometimes you have to travel the stars)", "https://upload.wikimedia.org/wikipedia/en/b/bd/H2G2_UK_front_cover.jpg", 9.99m, "The Hitchhiker's Guide to the Galaxy" });
+
+            migrationBuilder.InsertData(
+                table: "Products",
+                columns: new[] { "Id", "Description", "ImageUrl", "Price", "Title" },
+                values: new object[] { 2, "Ready Player One is a 2011 science fiction novel, and the debut novel for the best player in America", "https://upload.wikimedia.org/wikipedia/en/a/a4/Ready_Player_One_cover.jpg", 7.99m, "Ready Player One" });
+
+            migrationBuilder.InsertData(
+                table: "Products",
+                columns: new[] { "Id", "Description", "ImageUrl", "Price", "Title" },
+                values: new object[] { 3, "Ninteen Eighty-Four (also stylised as 1984) is a native store of USA background", "https://upload.wikimedia.org/wikipedia/commons/c/c3/1984first.jpg", 9.99m, "Ninteen Eighty-Four" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "Price",
+                table: "Products",
+                type: "decimal(18,2",
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(18,2)");
+        }
+    }
+}
